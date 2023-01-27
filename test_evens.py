8640ac0b23791fc27d4b6fce025d2cea510b59af
AndypSheridan <andy.p.sheridan@gmail.com>
2023-01-27 18:04:23 +0000

    Write first test to fail and then pass

--- test_evens.py
+++ test_evens.py
@@ -1,8 +1,11 @@
 import unittest
+from evens import even_number_of_evens
 
 
 class TestEvens(unittest.TestCase):
-    pass
+    def test_throws_error_if_value_passed_in_is_not_list(self):
+        self.assertRaises(TypeError, even_number_of_evens, 4)
 
 
-unittest.main()
+if __name__ == '__main__':
+    unittest.main()
