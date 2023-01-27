1d201901f0cda1167e10ba9a7c7d3968fef6d1ba
AndypSheridan <andy.p.sheridan@gmail.com>
2023-01-27 17:44:43 +0000

    Initial commit

---
+++ evens.py
@@ -0,0 +1,14 @@
+def even_number_of_evens(numbers):
+    """
+    Should raise a TypeError if a list is not passed into the function
+    error message: "A list was not passed into the function"
+    if numbers are empty return False
+    if the number of evens is odd, return False
+    if the number of evens is zero, return False
+    if the number of evens is even, return True
+    """
+
+    return None
+
+
+print(even_number_of_evens(5))
