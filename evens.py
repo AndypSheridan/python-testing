30ebae0cb33037a5f0bd104a53fa105c43a82401
AndypSheridan <andy.p.sheridan@gmail.com>
2023-01-30 18:13:50 +0000

    Add mocking tests

--- evens.py
+++ evens.py
@@ -9,10 +9,21 @@ def even_number_of_evens(numbers):
     """
 
     if isinstance(numbers, list):
-        return True
+        # if numbers == []:
+        #     return False
+        # else:
+        evens = sum([1 for n in numbers if n % 2 == 0])
+
+        # if evens:
+        #     return evens % 2 == 0
+        # else:
+        #     return False
+
+        return True if evens and evens % 2 == 0 else False
+
     else:
         raise TypeError("A list was not passed into the function")
 
 
 if __name__ == '__main__':
-    print(even_number_of_evens(5))
+    print(even_number_of_evens([2, 1, 4]))
